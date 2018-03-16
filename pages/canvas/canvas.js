981f649c0f0d20ac1119047b376277d2a8a201f2
JaimeCheng <403693553@qq.com>
2018-03-16 15:08:23 +0800

    no message

---
+++ pages/canvas/canvas.js
@@ -0,0 +1,113 @@
+// pages/canvas/canvas.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hidden: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    let promise1 = new Promise(function (resolve, reject) {
+      wx.getImageInfo({
+        src: '../../images/qrcode.jpg',
+        success: function (res) {
+          console.log(res)
+          resolve(res);
+        }
+      })
+    });
+    let promise2 = new Promise(function (resolve, reject) {
+      wx.getImageInfo({
+        src: '../../images/qrbg.png',
+        success: function (res) {
+          console.log(res)
+          resolve(res);
+        }
+      })
+    });
+    Promise.all([
+      promise1, promise2
+    ]).then(res => {
+      console.log(res)
+      const ctx = wx.createCanvasContext('shareImg')
+
+      //主要就是计算好各个图文的位置
+      ctx.drawImage('../../' + res[0].path, 158, 190, 210, 210)
+      ctx.drawImage('../../' + res[1].path, 0, 0, 545, 771)
+
+      ctx.setTextAlign('center')    
+      ctx.setFillStyle('#ffffff')  
+      ctx.setFontSize(22)        
+      ctx.fillText('分享文字描述1', 545 / 2, 130)
+      ctx.fillText('分享文字描述2', 545 / 2, 160)
+
+      ctx.stroke()
+      ctx.draw()
+    })
+  },
+
+
+ /**
+  * 生成分享图
+ */
+  share: function () {
+    var that = this
+    wx.showLoading({
+      title: '努力生成中...'
+    })
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 545,
+      height: 771,
+      destWidth: 545,
+      destHeight: 771,
+      canvasId: 'shareImg',
+      success: function (res) {
+        console.log(res.tempFilePath);
+        that.setData({
+          prurl: res.tempFilePath,
+          hidden: false
+        })
+        wx.hideLoading()
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 保存到相册
+  */
+  save: function () {
+    var that = this
+    //生产环境时 记得这里要加入获取相册授权的代码
+    wx.saveImageToPhotosAlbum({
+      filePath: that.data.prurl,
+      success(res) {
+        wx.showModal({
+          content: '图片已保存到相册，赶紧晒一下吧~',
+          showCancel: false,
+          confirmText: '好哒',
+          confirmColor: '#72B9C3',
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              that.setData({
+                hidden: true
+              })
+            }
+          }
+        })
+      }
+    })
+    
+  }
+})
